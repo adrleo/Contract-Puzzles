06f3663d2a07e29f92bd448483a731f497b54a3c
adrleo <adrleo247@gmail.com>
2025-02-28 14:00:16 +0700

    complete puzzles

--- test/game4Test.js
+++ test/game4Test.js
@@ -3,19 +3,23 @@ const { assert } = require('chai');
 
 describe('Game4', function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
 
-    return { game };
+    const signer1 = ethers.provider.getSigner(0);
+
+    return { game, signer1 };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-
+  it("should be a winner", async function () {
+    const { game, signer1 } = await loadFixture(deployContractAndSetVariables);
     // nested mappings are rough :}
+    const address1 = await signer1.getAddress();
 
-    await game.win();
+    await game.connect(signer1).write(address1);
+
+    await game.win(address1);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,39 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    const { Wallet } = require("ethers");
 
-    await game.win();
+    const threshold = BigInt("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf");
+    let wallet;
+    let addressNum;
+    while (true) {
+      wallet = Wallet.createRandom();
+      addressNum = BigInt(wallet.address);
+      if (addressNum < threshold) {
+        break;
+      }
+    }
+    wallet = wallet.connect(ethers.provider);
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.utils.parseEther("69"),
+    });
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
